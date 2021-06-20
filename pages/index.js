f08de10c2653fe8953a90da366ac628c5e2394f5
angelina-tsuboi <angelina.t1832@gmail.com>
2021-06-19 19:02:18 -0700

    feat: finish styling line chart

--- pages/index.js
+++ pages/index.js
@@ -31,17 +31,21 @@ const data2 = {
   labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
   datasets: [
     {
-      label: "First dataset",
+      label: "Adults",
       data: [33, 53, 85, 41, 44, 65],
-      fill: true,
-      backgroundColor: "rgba(75,192,192,0.2)",
+      fill: false,
       borderColor: "rgba(75,192,192,1)"
     },
     {
-      label: "Second dataset",
+      label: "Children",
       data: [33, 25, 35, 51, 54, 76],
       fill: false,
       borderColor: "#742774"
+    },{
+      label: "Infants",
+      data: [40, 55, 35, 51, 48, 76],
+      fill: false,
+      borderColor: "rgb(255,0,255)"
     }
   ]
 };
